d45172bdaee2b37725cd5a22b969d74012b06039
gitahievans <gitahievance@gmail.com>
2022-11-05 22:47:28 +0300

    add route in App.js to Contact page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import Links from "./Links";
 import Profile from "./Profile";
-
+import { Routes, Route } from "react-router-dom";
+import Contact from "../pages/Contact";
 function App() {
   return (
     <div className="app">
       <div className="container">
         <Profile />
         <Links />
+        <Routes>
+          <Route to="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </div>
   );
