be7b557300dc9cc17e6de77a6c5bd19555fc9d70
gitahievans <gitahievance@gmail.com>
2022-11-06 01:13:04 +0300

    Add and style footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <div className="foot">
+      <img
+        className="zuri-int"
+        src="https://zuri.team/img/zuri-internship-img.svg"
+        alt="zuri"
+      />
+
+      <p>HNG Internship 9 Frontend Task</p>
+      <img
+        className="ingressive"
+        src="https://ingressive.org/wp-content/uploads/2020/05/I4G-Logo-Color-Cropped.png"
+        alt="ingressive"
+      />
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Profile from "./Profile";
 import Links from "./Links";
+import Footer from "./Footer";
 
 function Layout() {
   return (
     <>
       <Profile />
       <Links />
+      <Footer />
     </>
   );
 }
