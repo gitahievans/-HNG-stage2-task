ea8f4af097852f559c1b61394491952b231b85b3
gitahievans <gitahievance@gmail.com>
2022-11-06 00:12:59 +0300

    styles email and message inputs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,27 +10,47 @@ function Contact() {
         <h1>Hi there, contact me to ask about anything you have in mind</h1>
         <form onSubmit={handleSubmit}>
           <div className="names">
-            <input
-              className="first"
-              type="text"
-              placeholder="Enter your last name"
-            />
-            <input
-              className="last"
-              type="text"
-              placeholder="Enter your last name"
-            />
+            <div className="first">
+              <label>First Name</label>
+              <input
+                className="inputs"
+                type="text"
+                placeholder="Enter your last name"
+              />
+            </div>
+            <div className="last">
+              <label>Last Name</label>
+              <input
+                className="inputs"
+                type="text"
+                placeholder="Enter your last name"
+              />
+            </div>
           </div>
           <div className="email-div">
             <label>Email</label>
             <input
-              className="email"
+              className="inputs"
+              type="text"
+              placeholder="yourname@email.com"
+            />
+          </div>
+
+          <div className="message-div">
+            <label>Message</label>
+            <input
+              className="message"
               type="text"
               placeholder="Send a message and I'll reply as soon as possible"
             />
           </div>
+          <div className="check-div">
+            <input className="check" type="checkbox" />
+            <label>
+              You agree to provide your data to Evans who may contact you
+            </label>
+          </div>
 
-          <input className="check" type="checkbox" />
           <button type="submit">Send Message</button>
         </form>
       </div>
